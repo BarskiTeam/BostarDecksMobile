a4d69054634d393e4660451216ac983a7159db24
Hamilkar247 <adonnibal96@gmail.com>
2020-12-29 20:12:04 +0100

    [ISS-14] using import all elements from xml file
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- app/src/main/java/com/example/bostardecksmobile/FirstFragment.kt
+++ app/src/main/java/com/example/bostardecksmobile/FirstFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
